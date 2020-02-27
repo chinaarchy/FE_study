3b2aa65289c92d52e6ddbbc100cc42dd4160b258
archy <chinaarchy@gmail.com>
2020-02-27 23:15:20 +0800

    before deploy

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -1,6 +1,3 @@
-const navConfig = require('./configs/navConfig')
-const siderbar = require('./configs/sidebarConfig')
-
 module.exports = {
     title: 'archy的博客',
     description: 'archy的博客，技术与生活',
@@ -10,9 +7,44 @@ module.exports = {
         }
     },
     head: ['link', {rel: 'icon', href: '/favicon.ico'}],
+    plugins: {
+        "@vssue/vuepress-plugin-vssue": {
+            platform: 'github',
+            owner: ''
+        }
+    },
+    base: 'FE_study',
     themeConfig: {
-        nav: navConfig,
-        sidebar: siderbar,
-        logo: 'logo.png'
+        smoothScroll: true,
+        dateFormat: 'YYYY-MM-DD',
+        nav: [
+            {
+                text: 'Blog',
+                link: '/'
+            }
+        ],
+        footer: {
+            contact: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/chinaarchy',
+                },
+                {
+                    type: 'mail',
+                    link: 'chinaarchy@icloud.com',
+                },
+            ],
+            copyright: [
+                {
+                    text: 'Privacy Policy',
+                    link: 'https://policies.google.com/privacy?hl=en-US',
+                },
+                {
+                    text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+                    link: '',
+                },
+            ]
+        }
+
     }
 }
---
+++ blogs/FE/README.md
@@ -0,0 +1,4 @@
+# 前端技术总结
+1. JavaScripr
+2. HTML
+3. CSS 
